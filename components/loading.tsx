97461ff06d15bb977db5470a6d5d5263e9ca9360
Josiah Yeow <josiahyeow.dev@gmail.com>
2022-04-19 20:53:22 +1000

    potato loader

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,12 +1,40 @@
-import { Zap } from "react-feather";
 import styled from "styled-components";
 
 const Container = styled.div`
   margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Potato = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  font-size: 2.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  animation-name: spin;
+  animation-duration: 1.5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 export const Loading = () => (
   <Container>
-    <Zap size="4rem" />
+    <Potato>🥔</Potato>
+    <div>baking the potato...</div>
   </Container>
 );
